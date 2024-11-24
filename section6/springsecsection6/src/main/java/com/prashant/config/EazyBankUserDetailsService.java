e5de0c5067db485649ef709970f2443755aff4dd
Prashant Saini <prashantsaini.code@gmail.com>
2024-11-24 19:06:50 +0530

    Added 1. Custom Authentication Provider Class 2. Different Profiles

---
+++ section6/springsecsection6/src/main/java/com/prashant/config/EazyBankUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.prashant.config;
+
+import com.prashant.model.Customer;
+import com.prashant.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// this is custom UserDetailsService implementation so spring security will know that
+// we are trying to use our Custom UDS instead of InMemoryUDS or JdbcUDS
+
+@Service    // bean of this class will be created
+@RequiredArgsConstructor   // this anno. will create the constructor with required field and autowire it automatically
+public class EazyBankUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+//    username - the username identifying the user whose data is required.
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // username here is received from DaoAuthentication Provider class
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("User details not found for the user:" + username));
+
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
